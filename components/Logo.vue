9b2a9700d373bb40d9f4a71f8980f208e58c0d22
andrealemonroy <andreale17@icloud.com>
2020-03-25 20:32:34 -0500

    initial commit

---
+++ components/Logo.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <bounce-loader
+      class="fixed ml-35"
+      :color="color"
+      :size="size"
+    ></bounce-loader>
+    <img class="absolute mt-34" src="../assets/icons/logo-white.png" />
+  </div>
+</template>
+<script>
+import BounceLoader from "vue-spinner/src/BounceLoader.vue";
+export default {
+  data() {
+    return {
+      color: "#ffffff",
+      size: "240px",
+      margin: "2px",
+      radius: "2px"
+    };
+  },
+  components: {
+    BounceLoader
+  }
+};
+</script>
+<style>
+.fixed {
+  position: fixed;
+}
+.absolute {
+  position: absolute;
+}
+.ml-35 {
+  margin-left: -35px;
+}
+.mt-34 {
+  margin-top: 34px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="bg-red full">
+    <Row type="flex" justify="space-around" align="middle">
+      <Col :xs="11" :sm="4" :md="4" :lg="3">
+        <Logo class="mt-50"></Logo>
+      </Col>
+    </Row>
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <Row type="flex" justify="space-around" align="bottom">
+      <Col :xs="16" :sm="14" :md="6" :lg="4">
+        <Button class="mt-50" @click="start">EMPECEMOS</Button>
+      </Col>
+    </Row>
+    <br />
+    <br />
+    <Row type="flex" justify="center" align="bottom">
+      <Col :xs="16" :sm="14" :md="6" :lg="4">
+        <p class="f-white ml-4 semi-bold">#YoMeQuedoEnCasa</p>
+      </Col>
+    </Row>
+  </div>
+</template>
+<script>
+import Logo from "~/components/Logo.vue";
+export default {
+  components: {
+    Logo
+  },
+  methods:{
+    start(){
+      this.$router.push({ path: route })
+    }
+    // start = () => {
+
+    // }
+  }
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css?family=Montserrat:400,500,600,900&display=swap");
+* {
+  font-family: "Montserrat";
+}
+.f-white {
+  color: white;
+}
+.ivu-btn {
+  color: #dc050e;
+  background-color: #ffffff;
+  border-radius: 30px;
+  padding: 1em 5em;
+  font-weight: 600;
+  letter-spacing: 2px;
+}
+.ivu-btn:hover {
+  color: #ffffff;
+  background-color: #dc050e;
+  border-color: #ffffff;
+  border-radius: 30px;
+  padding: 1em 5em;
+  font-weight: 600;
+  letter-spacing: 2px;
+}
+.bold {
+  font-weight: 900;
+}
+.semi-bold {
+  font-weight: 600;
+}
+.bg-red {
+  background-color: #dc050e;
+}
+.full {
+  min-height: 100vh;
+  min-width: 100vw;
+}
+.mt-50 {
+  margin-top: 50%;
+}
+.ml-4 {
+  margin-left: 4em;
+}
+</style>
