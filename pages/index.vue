eae00b31d9fdcbfa861ba299e56a56db57551554
fiorellasaro <fiorella.sfl@gmail.com>
2020-03-28 01:34:47 -0500

    index style

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-red full">
     <Row type="flex" justify="space-around" align="middle">
-      <Col :xs="11" :sm="4" :md="4" :lg="3">
+      <Col :xs="12" :sm="4" :md="4" :lg="3">
         <Logo class="mt-50"></Logo>
       </Col>
     </Row>
@@ -18,20 +18,16 @@
     <br />
     <br />
     <br />
-    <br />
-    <br />
-    <br />
+
     <Row type="flex" justify="space-around" align="bottom">
-      <Col :xs="16" :sm="14" :md="6" :lg="4">
+      <Col :xs="22" :sm="14" :md="6" :lg="4">
         <Button class="mt-50 f-mont" @click="start">EMPECEMOS</Button>
       </Col>
     </Row>
     <br />
     <br />
     <Row type="flex" justify="center" align="bottom">
-      <Col :xs="16" :sm="14" :md="6" :lg="4">
-        <p class="f-white ml-4 semi-bold f-mont">#YoMeQuedoEnCasa</p>
-      </Col>
+        <p class="f-white semi-bold f-mont">#YoMeQuedoEnCasa</p>
     </Row>
   </div>
 </template>
@@ -49,12 +45,16 @@ export default {
 
     // }
   },
-  head () {
+  head() {
     return {
       link: [
-        { href: 'https://fonts.googleapis.com/css?family=Montserrat&display=swap', rel: 'stylesheet' }
+        {
+          href:
+            "https://fonts.googleapis.com/css?family=Montserrat&display=swap",
+          rel: "stylesheet"
+        }
       ]
-    }
+    };
   }
 };
 </script>
@@ -62,8 +62,7 @@ export default {
 @import "../assets/style.css";
 @import url("https://fonts.googleapis.com/css?family=Montserrat:400,500,600,900&display=swap");
 
-
-.f-mont{
-   font-family: "Montserrat" !important;
+.f-mont {
+  font-family: "Montserrat" !important;
 }
 </style>
